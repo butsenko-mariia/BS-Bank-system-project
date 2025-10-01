99aa2c0974e3360cb2a97259437ab95ca131e524
Maria Butsenko <butsenko.mariia1123@vu.cdu.edu.ua>
2025-10-01 11:45:26 +0300

    BS-004-add-method-to-class-Client

--- src/main/java/program/App.java
+++ src/main/java/program/App.java
@@ -1,8 +1,12 @@
 package program;
 
+import program.Bank.Client;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to Bank System");
+        Client petrov = new Client();
+        petrov.Print();
 
     }
 
