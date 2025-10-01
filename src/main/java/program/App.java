e65609271078b8226c3c08efb2844671ae733829
Snizhana <herasymenko.snizhana1123@vu.cdu.edu.ua>
2025-10-01 12:31:34 +0300

    ReadFile

--- src/main/java/program/App.java
+++ src/main/java/program/App.java
@@ -1,13 +1,28 @@
 package program;
-
 import program.Bank.Client;
+
+import java.io.*;
+import java.io.File;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to Bank System");
-        Client petrov = new Client();
-        petrov.Print();
+        ReadFile();
+
 
     }
-
+private static void ReadFile(){
+    try {
+        File myObj = new File("C:/BankProject/DataBase/Clients.txt");
+        Scanner myReader = new Scanner(myObj);
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            System.out.println(data);
+        }
+        myReader.close();
+    } catch (FileNotFoundException e) {
+        System.out.println("Виникла помилка.");
+        e.printStackTrace();
+    }
+}
 }
