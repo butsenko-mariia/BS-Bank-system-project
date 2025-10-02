50b520a5b0f4898224a02044b30a08dffaf1868f
Maria Butsenko <butsenko.mariia1123@vu.cdu.edu.ua>
2025-10-02 13:09:58 +0300

    BS-007 create classes, write get/set

--- src/main/java/program/Bank/Client.java
+++ src/main/java/program/Bank/Client.java
@@ -47,11 +47,49 @@ public class Client
         this.place_of_work_or_study = scanner.nextLine();
         System.out.println("This client has been successfully registered");
     }
+    public String getId() {
+        return this.id;
+    }
+    public String getFullName() {
+        return this.full_name;
+    }
+    public String getDateOfBirth() {
+        return this.date_of_birth;
+    }
+    public String getSex() {
+        return this.sex;
+    }
+    public String getNationality() {
+        return this.nationality;
+    }
+    public void setMobilePhone(String mobile_phone) {
+        this.mobile_phone = mobile_phone;
+    }
+    public String getMobilePhone() {
+        return this.mobile_phone;
+    }
+    public int getIndividualTaxNumber() {
+        return this.individual_tax_number;
+    }
+    public int getPassportNumber() {
+        return this.individual_tax_number;
+    }
+    public  void setLegalAddress(String legal_address) {
+        this.legal_address = legal_address;
+    }
+    public String getLegalAddress() {
+        return this.legal_address;
+    }
+    public String getPlaceOfBirth() {
+        return this.place_of_birth;
+    }
+    public String getRecordNumber() {
+        return this.record_number;
+    }
+    public String getPlaceOfWorkOrStudy() {
+        return this.place_of_work_or_study;
+    }
     public void Print(){
-        System.out.printf("Database ID: %s\nFull name: %s\nDate of birth: %s\nContact phone number: %s\nGender: %s\n" +
-                        "Nationality: %s\nIndividual tax number: %d\nPassport number: %d\nRecord number: %s\nPlace of birth: %s\n" +
-                        "Registered address: %s\nPlace of work/study: %s\n",  id, full_name, date_of_birth,
-                sex, nationality, mobile_phone, individual_tax_number, passport_number, legal_address, place_of_birth,
-                record_number, place_of_work_or_study);
+        System.out.printf(this.toString());
     }
 }
