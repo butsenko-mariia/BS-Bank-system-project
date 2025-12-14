f665159a43af8420970921cba3821768f70e9715
Snizhana <herasymenko.snizhana1123@vu.cdu.edu.ua>
2025-12-14 14:57:30 +0200

    Add logging to Client and Card

--- src/main/java/program/Bank/Client.java
+++ src/main/java/program/Bank/Client.java
@@ -1,6 +1,9 @@
 package program.Bank;
 import program.Bank.Enums.ClientStatus;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
@@ -8,6 +11,8 @@ import java.util.UUID;
 
 public class Client
 {
+    private static Logger log = LogManager.getLogger(Client.class);
+
     private UUID id;
     private String full_name;
     private LocalDate date_of_birth;
@@ -23,11 +28,14 @@ public class Client
     private ClientStatus status;
 
     public Client(){
+        log.debug("Створення нового екземпляру Client");
         this.setId();
         this.status=ClientStatus.ACTIVE;
+        log.debug("Client створено зі статусом ACTIVE");
     }
 
     public Client(UUID id) {
+        log.debug("Створення екземпляру Client з існуючим ID: {}", id);
         this.setId(id);
     }
 
@@ -36,17 +44,23 @@ public class Client
     }
 
     public void setId() {
+        log.info("Спроба генерації нового ID клієнта");
         if (id != null) {
+            log.error("ID клієнта вже встановлено: {}", id);
             throw  new IllegalStateException("Client ID is already set");
         }
         this.id = UUID.randomUUID();
+        log.info("ID клієнта успішно згенеровано: {}", id);
     }
 
     public void setId(UUID id) {
+        log.info("Встановлення існуючого ID клієнта: {}", id);
         if (id == null) {
+            log.error("Спроба встановити null як ID клієнта");
             throw  new NumberFormatException("ID must be not null");
         }
         this.id =  id;
+        log.debug("ID клієнта успішно встановлено");
     }
 
     public String getFull_name() {
@@ -54,13 +68,17 @@ public class Client
     }
 
     public void setFull_name(String full_name) {
+        log.info("Встановлення повного імені: {}", full_name);
         if (full_name == null || full_name.isEmpty()) {
+            log.error("Повне ім'я порожнє або null");
             throw new IllegalArgumentException("Full name cannot be empty.");
         }
         if (!full_name.matches("^[A-Za-zА-Яа-яІіЇїЄє'\\-\\s]+$")) {
+            log.error("Невірний формат повного імені (містить цифри чи спец. символи): {}", full_name);
             throw new IllegalArgumentException("Full name cannot contain numbers or special symbols.");
         }
         this.full_name = full_name.trim();
+        log.debug("Повне ім'я успішно встановлено: {}", this.full_name);
     }
 
     public LocalDate getDate_of_birth() {
@@ -68,9 +86,13 @@ public class Client
     }
 
     public void setDate_of_birth(LocalDate date_of_birth) {
-        if (date_of_birth == null)
+        log.info("Встановлення дати народження: {}", date_of_birth);
+        if (date_of_birth == null) {
+            log.error("Дата народження є null");
             throw new IllegalArgumentException("Date of birth must be in format dd.mm.yyyy.");
+        }
         this.date_of_birth = date_of_birth;
+        log.debug("Дату народження успішно встановлено");
     }
 
     public String getSex() {
@@ -78,10 +100,13 @@ public class Client
     }
 
     public void setSex(String sex) {
+        log.info("Встановлення статі: {}", sex);
         if (!sex.equalsIgnoreCase("M") && !sex.equalsIgnoreCase("W")) {
+            log.error("Невірне значення статі: {}", sex);
             throw new IllegalArgumentException("Gender must be 'M' or 'W'.");
         }
         this.sex = sex.toUpperCase();
+        log.debug("Стать успішно встановлено: {}", this.sex);
     }
 
     public String getNationality() {
@@ -89,9 +114,13 @@ public class Client
     }
 
     public void setNationality(String nationality) {
-        if (nationality == null || nationality.trim().isEmpty())
+        log.info("Встановлення національності: {}", nationality);
+        if (nationality == null || nationality.trim().isEmpty()) {
+            log.error("Національність порожня або null");
             throw new IllegalArgumentException("Nationality cannot be empty.");
+        }
         this.nationality = nationality.trim();
+        log.debug("Національність успішно встановлено: {}", this.nationality);
     }
 
     public String getMobile_phone() {
@@ -99,9 +128,13 @@ public class Client
     }
 
     public void setMobile_phone(String mobile_phone) {
-        if (mobile_phone == null || !mobile_phone.matches("^\\+380\\d{9}$"))
+        log.info("Встановлення мобільного телефону: {}", mobile_phone);
+        if (mobile_phone == null || !mobile_phone.matches("^\\+380\\d{9}$")) {
+            log.error("Невірний формат номеру телефону: {}", mobile_phone);
             throw new IllegalArgumentException("Phone number must be in format +380XXXXXXXXX.");
+        }
         this.mobile_phone = mobile_phone;
+        log.debug("Номер телефону успішно встановлено");
     }
 
     public String getIndividual_tax_number() {
@@ -109,9 +142,13 @@ public class Client
     }
 
     public void setIndividual_tax_number(String individual_tax_number) {
-        if (individual_tax_number == null || individual_tax_number.trim().isEmpty())
+        log.info("Встановлення індивідуального податкового номеру (ІПН)");
+        if (individual_tax_number == null || individual_tax_number.trim().isEmpty()) {
+            log.error("ІПН порожній або null");
             throw new IllegalArgumentException("Individual tax  number cannot be empty.");
+        }
         this.individual_tax_number = individual_tax_number;
+        log.debug("ІПН успішно встановлено: {}", this.individual_tax_number);
     }
 
     public String getPassport_number() {
@@ -119,9 +156,13 @@ public class Client
     }
 
     public void setPassport_number(String passport_number) {
-        if (passport_number == null || passport_number.trim().isEmpty())
+        log.info("Встановлення номеру паспорта");
+        if (passport_number == null || passport_number.trim().isEmpty()) {
+            log.error("Номер паспорта порожній або null");
             throw new IllegalArgumentException("Passport number cannot be empty.");
+        }
         this.passport_number = passport_number;
+        log.debug("Номер паспорта успішно встановлено: {}", this.passport_number);
     }
 
     public String getRecord_number() {
@@ -129,9 +170,13 @@ public class Client
     }
 
     public void setRecord_number(String record_number) {
-        if (record_number == null || record_number.trim().isEmpty())
+        log.info("Встановлення номеру запису");
+        if (record_number == null || record_number.trim().isEmpty()) {
+            log.error("Номер запису порожній або null");
             throw new IllegalArgumentException("Record number cannot be empty.");
+        }
         this.record_number = record_number.trim();
+        log.debug("Номер запису успішно встановлено: {}", this.record_number);
     }
 
     public String getPlace_of_birth() {
@@ -139,18 +184,28 @@ public class Client
     }
 
     public void setPlace_of_birth(String place_of_birth) {
-        if (place_of_birth == null || place_of_birth.trim().isEmpty())
+        log.info("Встановлення місця народження: {}", place_of_birth);
+        if (place_of_birth == null || place_of_birth.trim().isEmpty()) {
+            log.error("Місце народження порожнє або null");
             throw new IllegalArgumentException("Place of birth cannot be empty.");
+        }
         this.place_of_birth = place_of_birth.trim();
+        log.debug("Місце народження успішно встановлено: {}", this.place_of_birth);
+
     }
+
     public String getLegal_address() {
         return legal_address;
     }
 
     public void setLegal_address(String legal_address) {
-        if (legal_address == null || legal_address.trim().isEmpty())
+        log.info("Встановлення юридичної адреси: {}", legal_address);
+        if (legal_address == null || legal_address.trim().isEmpty()) {
+            log.error("Юридична адреса порожня або null");
             throw new IllegalArgumentException("Address cannot be empty.");
+        }
         this.legal_address = legal_address.trim();
+        log.debug("Юридичну адресу успішно встановлено: {}", this.legal_address);
     }
 
     public String getPlace_of_work_or_study() {
@@ -158,9 +213,13 @@ public class Client
     }
 
     public void setPlace_of_work_or_study(String place_of_work_or_study) {
-        if (place_of_work_or_study == null || place_of_work_or_study.trim().isEmpty())
+        log.info("Встановлення місця роботи/навчання: {}", place_of_work_or_study);
+        if (place_of_work_or_study == null || place_of_work_or_study.trim().isEmpty()) {
+            log.error("Місце роботи/навчання порожнє або null");
             throw new IllegalArgumentException("Place of work/study cannot be empty.");
+        }
         this.place_of_work_or_study = place_of_work_or_study.trim();
+        log.debug("Місце роботи/навчання успішно встановлено: {}", this.place_of_work_or_study);
     }
 
     public ClientStatus getStatus() {
@@ -168,7 +227,7 @@ public class Client
     }
 
     public void setStatus(ClientStatus status) {
-
+        log.info("Встановлення статусу клієнта: {}", status);
         boolean ifMatchStatus = false;
         for (ClientStatus clientStatus : ClientStatus.values()) {
             if (status == clientStatus) {
@@ -179,23 +238,30 @@ public class Client
         }
 
         if (!ifMatchStatus) {
+            log.error("Невірний статус клієнта: {}", status);
             throw new IllegalArgumentException("Invalid client status.");
         }
+        log.debug("Статус клієнта успішно встановлено: {}", this.status);
     }
 
     public BigDecimal getBalance() {
+        log.debug("Розрахунок загального балансу клієнта ID: {}", id);
         BigDecimal balance = new BigDecimal(0);
         //тут має бути функція яка рахує суму на всії рахунках клієнта
+        log.debug("Розрахований баланс клієнта: {}", balance);
         return balance;
     }
 
     public void Fetch(){
+        log.info("Завантаження даних клієнта з бази даних за ID: {}", id);
         //тут має бути метод який підтягує всю інформацію про об'єкт з бази даних за його ід та задає полям
         // даного екземпляру класу значення
+        log.debug("Дані клієнта успішно завантажено з БД");
     }
 
     @Override
     public String toString() {
+        log.debug("Перетворення даних клієнта в текстове представлення");
         return String.format(
                 "Сlient id = " + this.getId() + "\n" +
                         ",\nFull name = " + this.getFull_name() +
@@ -213,10 +279,13 @@ public class Client
     }
 
     public void PrintClientFullInfo(){
+        log.info("Виведення повної інформації про клієнта ID: {}", id);
         System.out.printf(this.toString());
+        log.debug("Повну інформацію про клієнта виведено");
     }
 
     public void PrintClientAccountInfo(){
+        log.info("Виведення інформації про рахунки клієнта ID: {}", id);
         String info = "Вітаємо, Іван Петрович!\n" +
                 "Статус: "+ this.getStatus() +"\n" +
                 "Баланс: "+ this.getBalance() +" грн\n" +
@@ -225,36 +294,49 @@ public class Client
         //+ "UAH (#acc001) - 5,000 грн\n" +
         //"USD (#acc002) - 1,200 $ ";
         System.out.printf(info);
+        log.debug("Інформацію про рахунки клієнта виведено");
     }
 
     public void PrintClientTransactionHistory(){
+        log.info("Виведення історії транзакцій клієнта ID: {}", id);
         //тут має бути функція яка за номером ід клієнта знаходить усі транзакції з усіх рахунків даного клієнта
         String info = "";
         System.out.printf(info);
+        log.debug("Історію транзакцій клієнта виведено");
     }
 
     public void PrintClientActiveDeposit(){
+        log.info("Виведення активних депозитів клієнта ID: {}", id);
         //тут має бути функція яка перебирає усі наявні депозити за номером ід клієнта в базі даних
         //які є активними та виводить інфу про них
+        log.debug("Активні депозити клієнта виведено");
     }
 
     public void PrintClientDepositHistory(){
+        log.info("Виведення історії депозитів клієнта ID: {}", id);
         //тут має бути функція яка перебирає усі наявні депозити за номером ід клієнта в базі даних
         //та виводить інфу про кожен з них в порядку по даті створення
+        log.debug("Історію депозитів клієнта виведено");
     }
 
     public void PrintClientActiveCredit(){
+        log.info("Виведення активних кредитів клієнта ID: {}", id);
         //тут має бути функція яка перебирає усі наявні кредити за номером ід клієнта в базі даних
         //які є активними та виводить інфу про них
+        log.debug("Активні кредити клієнта виведено");
     }
 
     public void PrintClientCreditHistory(){
+        log.info("Виведення історії кредитів клієнта ID: {}", id);
         //тут має бути функція яка перебирає усі наявні кредити за номером ід клієнта в базі даних
         //та виводить інфу про кожен з них в порядку по даті створення
+        log.debug("Історію кредитів клієнта виведено");
     }
 
     public void PrintClientActiveCard(){
+        log.info("Виведення активних карток клієнта ID: {}", id);
         //тут має бути функція яка перебирає усі наявні кредити за номером ід клієнта в базі даних
         //які є активними та виводить інфу про них
+        log.debug("Активні картки клієнта виведено");
     }
 }
