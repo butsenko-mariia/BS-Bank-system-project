086208648c96d604397ac5e809d06292c250eaa9
Snizhana <herasymenko.snizhana1123@vu.cdu.edu.ua>
2025-12-17 20:28:22 +0200

    add class dbconnection

---
+++ src/main/java/program/Bank/DBConnection.java
@@ -0,0 +1,15 @@
+package program.Bank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/BankSystem";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "ВАШПАРОЛЬ";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
