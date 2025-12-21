78ab1cf23de47fb7bcc0ab2b6a8d01d2bb9563e3
Maria Butsenko <butsenko.mariia1123@vu.cdu.edu.ua>
2025-12-21 23:58:15 +0200

    BS-039 fix composite pattern

--- src/main/java/program/Bank/Menu/Menu.java
+++ src/main/java/program/Bank/Menu/Menu.java
@@ -1,63 +1,94 @@
 package program.Bank.Menu;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import program.Bank.CapitalizationDeposit;
+import java.util.*;
+//This is a conyeiner of composite pattern
+public class Menu extends MenuComponent {
+    private final List<MenuComponent> commands = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
+    private final Logger log = LogManager.getLogger(Menu.class);
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
-
-public class Menu implements Command {
-    private final String name;
-    private final Scanner scanner;
-    // Використовуємо LinkedHashMap, щоб зберігати порядок команд при виводі
-    private final Map<String, Command> commands = new LinkedHashMap<>();
-
-    // КОНСТРУКТОР
-    public Menu(String name, Scanner scanner) {
-        this.name = name;
-        this.scanner = scanner;
-    }
-
-    // Метод додавання команд (або підменю) в це меню [cite: 76]
-    public void add(Command command) {
-        commands.put(command.name(), command);
+    public Menu(String name) {
+        super(name);
     }
 
     @Override
     public Result execute() {
         if (commands.isEmpty()) {
-            System.out.println("Menu is empty. Returning");
-            return Result.CONTINUE;
+            String mes = "Menu is empty. Returning";
+            log.warn(mes);
+            System.out.println(mes);
+            return Result.RETURN;
         }
 
-        Result result;
+        Result result = Result.CONTINUE;
+
         do {
-            prompt(); // Вивід списку команд
-            String commandName = scanner.nextLine().trim();
+            prompt();
 
-            Command command = commands.get(commandName);
+            int commandName;
 
-            if (command != null) {
-                result = command.execute(); // Виконання команди
-            } else {
-                System.out.println("Command not found. Try again");
-                result = Result.CONTINUE;
+            try {
+                commandName = Integer.parseInt(scanner.nextLine());
+            }
+            catch (NumberFormatException e) {
+                String mes = "Invalid input. Enter a number.";
+                log.warn(mes);
+                System.out.println(mes);
+                continue;
             }
 
-            // Працюємо, поки команда повертає CONTINUE [cite: 69]
+            if (commandName == 0) {
+                String mes = "User commanded to return. Returning...";
+                log.info(mes);
+                return Result.RETURN;
+            }
+            else if (commandName > 0 && commandName <= commands.size()){
+                MenuComponent command = commands.get(commandName - 1);
+                Result childResult = command.execute();
+
+                if (childResult == Result.EXIT) {
+                    return Result.EXIT;
+                }
+            }
+            else{
+                String mes = "Command not found. Try again";
+                log.warn(mes);
+                System.out.println(mes);
+            }
+
         } while (result == Result.CONTINUE);
 
-        // Якщо прийшов EXIT - передаємо далі, якщо RETURN - зупиняємо це меню
-        return result == Result.EXIT ? Result.EXIT : Result.CONTINUE;
+        return result;
     }
 
-    @Override
-    public String name() {
-        return name;
-    }
-
-    private void prompt() {
+    private void prompt(){
         System.out.println("\n=== " + name.toUpperCase() + " ===");
-        System.out.println("Available commands: " + commands.keySet());
-        System.out.print("> ");
+        StringBuilder commandNames = new StringBuilder();
+        for (int i =  0; i < commands.size(); i++){
+            commandNames.append(i+1).append(". ").append(commands.get(i).getName()).append("\n");
+        }
+        System.out.println("Enter one of the available commands: "+commandNames+"\n");
     }
+
+    public void add(MenuComponent command) {
+        this.commands.add(command);
+        String mes = command.getName() + " has been added";
+        log.debug(mes);
+    }
+
+    public void remove(MenuComponent command) {
+        this.commands.remove(command);
+        String mes = command.getName() + " has been removed";
+        log.debug(mes);
+    }
+
+    public void remove(int index) {
+        this.commands.remove(index);
+        String mes = "Command with index["+ index +"] has been removed";
+        log.debug(mes);
+    }
+
 }
--- src/main/java/program/Bank/Menu/Result.java
+++ src/main/java/program/Bank/Menu/Result.java
@@ -1,8 +1,7 @@
 package program.Bank.Menu;
 
-
 public enum Result {
-    EXIT,       // Повний вихід з програми
-    RETURN,     // Повернення в попереднє меню
-    CONTINUE    // Продовжити роботу в поточному меню
+    EXIT,
+    RETURN,
+    CONTINUE
 }
