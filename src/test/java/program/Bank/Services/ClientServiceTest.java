051065f772a8dcd0a2489347038060c995d4f17d
Snizhana <herasymenko.snizhana1123@vu.cdu.edu.ua>
2025-12-23 19:02:27 +0200

    BS-037 Create Mockito test part 1

---
+++ src/test/java/program/Bank/Services/ClientServiceTest.java
@@ -0,0 +1,36 @@
+package program.Bank.Services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import program.Bank.Client;
+import program.Bank.DataBase;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class ClientServiceTest {
+
+    @Mock
+    private DataBase dataBase;
+
+    @InjectMocks
+    private ClientService clientService;
+
+    @Test
+    void shouldRegisterClient() {
+        // 1. Дія: Спробуємо зареєструвати клієнта
+        clientService.RegisterClient(
+                "Taras Test", "2000-01-01", "Male", "UA",
+                "0991234567", "1111111111", "AA000000",
+                "Kyiv", "Kyiv", "rec1", "Work"
+        );
+
+        // 2. Перевірка: Чи зберіг сервіс клієнта в базу?
+        verify(dataBase, times(1)).Upload(any(Client.class));
+    }
+}
